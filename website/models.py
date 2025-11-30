fb09bc16b7e292123d3938c1381fb04ca8bc99e4
Amin <aminkargarzadeh26@gmail.com>
2025-11-30 10:23:52 +0330

    Contact model
    Contact model Completion!

--- website/models.py
+++ website/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Contact(models.Model):
     name = models.CharField(max_length=255)
@@ -8,3 +9,9 @@ class Contact(models.Model):
     message = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ('-created_date',)
+    
+    def __str__(self):
+        return self.name

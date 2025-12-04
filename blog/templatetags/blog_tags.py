713023d0da4271037ca58bba038b94e2b3774338
Amin <aminkargarzadeh26@gmail.com>
2025-12-04 16:59:07 +0330

    Simple tag
    Creating a simple tag that returns the count of the posts!

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,12 @@
 from django import template
+from blog.models import Post
 
 register = template.Library()
+
+@register.simple_tag(name='totalposts')
+def function():
+    return Post.objects.filter(status=1).count()
+
+@register.simple_tag(name='posts')
+def function2():
+    return Post.objects.filter(status=1)

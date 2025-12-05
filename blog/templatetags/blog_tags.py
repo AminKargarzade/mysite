0bfd74a0fc22932160f0e99446df045b0ce999c4
Amin <aminkargarzadeh26@gmail.com>
2025-12-05 14:29:47 +0330

    Categories
    Completed the sidebar Categories with inclusion template tag with a good code!

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Post
+from blog.models import Category
 
 register = template.Library()
 
@@ -19,3 +20,12 @@ def snippet(value,arg=20):  # sourcery skip: use-fstring-for-concatenation
 def latest_posts(arg=3):
     posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
     return {'posts': posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def post_categories():  # sourcery skip: dict-comprehension
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name]=posts.filter(category=name).count()
+    return {'categories': cat_dict}

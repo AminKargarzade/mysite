35830580dd9efb6973edfb7ea3633a57a1a34d6d
Amin <aminkargarzadeh26@gmail.com>
2025-12-11 16:08:28 +0330

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('about', about_view, name="about"),
     path('contact', contact_view, name="contact"),
     path('test', test_view, name="test"),
-    path('newsletter', newsletter_view, name="newsletter"),
+    path('newsletter', newsletter_view, name="newsletter") # type: ignore
 ]

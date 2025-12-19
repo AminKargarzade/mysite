ef5985fba3c63c8ce5c15222d6ea691b81be2c8f
Amin <aminkargarzadeh26@gmail.com>
2025-12-20 00:06:40 +0330

    Comment Form
    Created Comments Form!

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post,Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from blog.forms import CommentForm
+from django.contrib import messages
 
 # Create your views here.
 def blog_view(request,**kwargs):
@@ -29,11 +31,20 @@ def blog_view(request,**kwargs):
 
 
 def blog_single(request,pid):
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Your comment has been submitted successfully and is awaiting approval.')
+        else:
+            messages.add_message(request, messages.ERROR, 'There was an error submitting your comment. Please try again.')
+        
     posts = Post.objects.filter(status=1)
     # post = get_object_or_404(Post, pk=pid, status=1)
     post = get_object_or_404(posts,pk=pid)
     comments = Comment.objects.filter(post=post.id, approved=True)
-    context = {'post':post, 'comments':comments}
+    form = CommentForm()
+    context = {'post':post, 'comments':comments, 'form':form}
     return render(request, 'blog/blog-single.html', context)
 
 

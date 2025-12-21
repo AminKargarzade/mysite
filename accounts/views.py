11d7cd9ec754062921f5641ef8d3ebd400e352f6
Amin <aminkargarzadeh26@gmail.com>
2025-12-21 17:28:19 +0330

    Login Form
    Implementing the login Form from bootstrap

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')  # Redirect to a success page.
     return render(request, 'accounts/login.html')
 def logout_view(request):
     pass  # Implement logout logic here
